85142277819f20db35a590e0d3ea2110ed93a6eb
EtienneCmb <e.combrisson@gmail.com>
2017-06-24 19:16:34 +0200

    Start travis and coverage integration

--- setup.py
+++ setup.py
@@ -1,30 +1,65 @@
-"""
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
+import pip
+from pip.req import parse_requirements
+from optparse import Option
+
+__version__ = "0.5.0"
+NAME = 'Tensorpac'
+AUTHOR = "Etienne Combrisson"
+MAINTAINER = "Etienne Combrisson"
+EMAIL = 'e.combrisson@gmail.com'
+KEYWORDS = "phase-amplitude-coupling pac tensor"
+DESCRIPTION = "Tensor-based Phase-Amplitude Coupling"
+URL = 'http://etiennecmb.github.io/tensorpac/'
+DOWNLOAD_URL = 'https://github.com/EtienneCmb/tensorpac/archive/v' + __version__ + '.tar.gz'
+# Data path :
+PACKAGE_DATA = {}
 
 
 def read(fname):
+    """Read README and LICENSE."""
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+options = Option('--workaround')
+options.skip_requirements_regex = None
+REQ_FILE = './requirements.txt'
+# Hack for old pip versions: Versions greater than 1.x
+# have a required parameter "sessions" in parse_requierements
+if pip.__version__.startswith('1.'):
+    install_reqs = parse_requirements(REQ_FILE, options=options)
+else:
+    from pip.download import PipSession  # pylint:disable=E0611
+    options.isolated_mode = False
+    install_reqs = parse_requirements(REQ_FILE,  # pylint:disable=E1123
+                                      options=options,
+                                      session=PipSession)
+
+REQS = [str(ir.req) for ir in install_reqs]
+
 setup(
-    name='tensorpac',
-    version='0.0',
+    name=NAME,
+    version=__version__,
     packages=find_packages(),
-    description='Tensor-based Phase-Amplitude Coupling Python toolbox',
-    long_description=read('README.md'),
-    install_requires=[
-        'numpy',
-        'joblib',
-    ],
-    dependency_links=[],
-    author='Etienne Combrisson',
-    maintainer='Etienne Combrisson',
-    author_email='e.combrisson@gmail.com',
-    url='https://github.com/EtienneCmb/tensorpac',
-    license=read('LICENSE'),
+    package_dir={'tensorpac': 'tensorpac'},
+    package_data=PACKAGE_DATA,
     include_package_data=True,
-    keywords='phase-amplitude coupling PAC connectivity tensor neuroscience',
+    description=DESCRIPTION,
+    long_description=read('README.rst'),
+    platforms='any',
+    setup_requires=['numpy', 'joblib'],
+    install_requires=REQS,
+    dependency_links=[],
+    author=AUTHOR,
+    maintainer=MAINTAINER,
+    author_email=EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    license=read('LICENSE'),
+    keywords=KEYWORDS,
     classifiers=["Development Status :: 3 - Alpha",
                  'Intended Audience :: Science/Research',
                  'Intended Audience :: Education',
