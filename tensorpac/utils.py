c738fc89412ead2bcbaff86c73c81e62c70d8a88
EtienneCmb <e.combrisson@gmail.com>
2019-06-19 13:36:28 +0200

    Fix data generation

--- tensorpac/utils.py
+++ tensorpac/utils.py
@@ -7,8 +7,6 @@ import numpy as np
 
 from tensorpac.spectral import morlet
 
-__all__ = ('pac_signals_wavelet', 'pac_signals_tort', 'pac_vec', 'pac_trivec')
-
 
 ###############################################################################
 ###############################################################################
@@ -59,14 +57,14 @@ def pac_signals_wavelet(f_pha=10., f_amp=100., sf=1024., n_pts=4000.,
     sf = float(sf)
     f_pha, f_amp = np.asarray(f_pha).mean(), np.asarray(f_amp).mean()
     # time = np.mgrid[0:n_trials, 0:n_pts][1] / sf
-    time = np.arange(n_pts).reshape(1, 1, -1) / sf
+    time = np.arange(n_pts) / sf
     # Random state of the machine :
     rng = np.random.RandomState(rnd_state)
     # Get complex decomposition of random points in the phase frequency band :
     driver = morlet(rng.randn(n_trials, n_channels, n_pts), sf, f_pha, axis=2)
     driver /= np.max(driver, axis=2, keepdims=True)
     # Create amplitude signals :
-    xh = np.sin(2 * np.pi * f_amp * time)
+    xh = np.sin(2 * np.pi * f_amp * time.reshape(1, 1, -1))
     dpha = np.exp(-1j * pp)
     modulation = 1. / (1. + np.exp(- 6. * 1. * np.real(driver * dpha)))
     # Modulate the amplitude :
@@ -76,40 +74,43 @@ def pac_signals_wavelet(f_pha=10., f_amp=100., sf=1024., n_pts=4000.,
     # Build the pac signal :
     data = xh + xl + noise * rng.randn(*xh.shape)
 
-    return data, time[0, :]
+    return data, time
 
 
-def pac_signals_tort(fpha=10., famp=100., sf=1024, npts=4000, ntrials=10,
-                     chi=0., noise=1., dpha=0., damp=0., rnd_state=0):
+def pac_signals_tort(f_pha=10., f_amp=100., sf=1024, n_pts=4000, n_trials=10,
+                     n_channels=1, chi=0., noise=1., dpha=0., damp=0.,
+                     rnd_state=0):
     """Generate artificially phase-amplitude coupled signals.
 
     This function use the definition of Tort et al. 2010.
 
     Parameters
     ----------
-    fpha : float | 10.
+    f_pha : float | 10.
         Frequency for phase. Use either a float number for a centered frequency
         of a band (like [5, 7]) for a bandwidth.
-    famp : float | 100.
+    f_amp : float | 100.
         Frequency for amplitude. Use either a float number for a centered
         frequency of a band (like [60, 80]) for a bandwidth.
     sf : int | 1024
         Sampling frequency
-    ntrials : int | 10
+    n_trials : int | 10
         Number of datasets
-    npts : int | 4000
+    n_pts : int | 4000
         Number of points for each signal.
+    n_channels : int | 1
+        Number of channels
     chi : float | 0.
         Amount of coupling. If chi=0, signals of phase and amplitude
         are strongly coupled (0.<=chi<=1.).
     noise : float | 1.
         Amount of noise (0<=noise<=3).
     dpha : float | 0.
-        Random incertitude on phase frequences (0<=dpha<=100). If fpha is 2,
+        Random incertitude on phase frequences (0<=dpha<=100). If f_pha is 2,
         and dpha is 50, the frequency for the phase signal will be between :
         [2-0.5*2, 2+0.5*2]=[1,3]
     damp : float | 0.
-        Random incertitude on amplitude frequencies (0<=damp<=100). If famp is
+        Random incertitude on amplitude frequencies (0<=damp<=100). If f_amp is
         60, and damp is 10, the frequency for the amplitude signal will be
         between : [60-0.1*60, 60+0.1*60]=[54,66]
     rnd_state: int | 0
@@ -118,45 +119,37 @@ def pac_signals_tort(fpha=10., famp=100., sf=1024, npts=4000, ntrials=10,
     Returns
     -------
     data : array_like
-        Array of pac signals of shape (ntrials, npts).
+        Array of signals of shape (n_trials, n_channels, n_pts).
     time : array_like
-        Time vector of shape (npts,).
+        Time vector of shape (n_pts,).
     """
-    npts = int(npts)
-    sf = float(sf)
+    n_pts, sf = int(n_pts), float(sf)
     # Check the inputs variables :
-    if not 0 <= chi <= 1:
-        chi = 0
-    if not 0 <= noise <= 3:
-        noise = 0
-    if not 0 <= dpha <= 100:
-        dpha = 0
-    if not 0 <= damp <= 100:
-        damp = 0
-    fpha, famp = np.asarray(fpha), np.asarray(famp)
-    time = np.mgrid[0:ntrials, 0:npts][1] / sf
-    data = np.zeros_like(time)
+    chi = 0 if not 0 <= chi <= 1 else chi
+    noise = 0 if not 0 <= noise <= 3 else noise
+    dpha = 0 if not 0 <= dpha <= 100 else dpha
+    damp = 0 if not 0 <= damp <= 100 else damp
+    f_pha, f_amp = np.asarray(f_pha), np.asarray(f_amp)
+    # time = np.mgrid[0:n_trials, 0:n_pts][1] / sf
+    time = np.arange(n_pts) / sf
     # Random state of the machine :
     rng = np.random.RandomState(rnd_state)
     # Band / Delta parameters :
-    sh = (ntrials, 1)
-    if fpha.ndim == 0:
-        apha = [fpha * (1 - dpha / 100), fpha * (1 + dpha / 100)]
+    sh = (n_trials, n_channels, 1)
+    if f_pha.ndim == 0:
+        apha = [f_pha * (1. - dpha / 100.), f_pha * (1. + dpha / 100.)]
         del_pha = apha[0] + (apha[1] - apha[0]) * rng.rand(*sh)
-    elif fpha.ndim == 1:
-        del_pha = rng.uniform(fpha[0], fpha[1], ntrials)
-    if famp.ndim == 0:
-        a_amp = [famp * (1 - damp / 100), famp * (1 + damp / 100)]
+    elif f_pha.ndim == 1:
+        del_pha = rng.uniform(f_pha[0], f_pha[1], (n_trials, n_channels, 1))
+    if f_amp.ndim == 0:
+        a_amp = [f_amp * (1. - damp / 100.), f_amp * (1. + damp / 100.)]
         del_amp = a_amp[0] + (a_amp[1] - a_amp[0]) * rng.rand(*sh)
-    elif famp.ndim == 1:
-        del_amp = rng.uniform(famp[0], famp[1], ntrials)
-
-    # Reshape phase/amplitude bands :
-    del_pha, del_amp = del_pha.reshape(-1, 1), del_amp.reshape(-1, 1)
+    elif f_amp.ndim == 1:
+        del_amp = rng.uniform(f_amp[0], f_amp[1], (n_trials, n_channels, 1))
 
     # Create phase and amplitude signals :
-    xl = np.sin(2 * np.pi * del_pha * time)
-    xh = np.sin(2 * np.pi * del_amp * time)
+    xl = np.sin(2 * np.pi * del_pha * time.reshape(1, 1, -1))
+    xh = np.sin(2 * np.pi * del_amp * time.reshape(1, 1, -1))
 
     # Create the coupling :
     ah = .5 * ((1. - chi) * xl + 1. + chi)
@@ -166,7 +159,7 @@ def pac_signals_tort(fpha=10., famp=100., sf=1024, npts=4000, ntrials=10,
     data = (ah * xh) + (al * xl)
     data += noise * rng.rand(*data.shape)  # Add noise
 
-    return data, time[0, :]
+    return data, time
 
 ###############################################################################
 ###############################################################################
@@ -175,12 +168,12 @@ def pac_signals_tort(fpha=10., famp=100., sf=1024, npts=4000, ntrials=10,
 ###############################################################################
 
 
-def pac_vec(fpha=(2, 30, 2, 1), famp=(60, 200, 10, 5)):
+def pac_vec(f_pha=(2, 30, 2, 1), f_amp=(60, 200, 10, 5)):
     """Generate cross-frequency coupling vectors.
 
     Parameters
     ----------
-    fpha, famp : tuple | (2, 30, 2, 1), (60, 200, 10, 5)
+    f_pha, f_amp : tuple | (2, 30, 2, 1), (60, 200, 10, 5)
         Frequency parameters for phase and amplitude. Each argument inside the
         tuple mean (starting fcy, ending fcy, bandwidth, step).
 
@@ -190,7 +183,7 @@ def pac_vec(fpha=(2, 30, 2, 1), famp=(60, 200, 10, 5)):
         Arrays containing the pairs of phase and amplitude frequencies. Each
         vector have a shape of (N, 2).
     """
-    return _check_freq(fpha), _check_freq(famp)
+    return _check_freq(f_pha), _check_freq(f_amp)
 
 
 def _check_freq(f):
@@ -202,7 +195,7 @@ def _check_freq(f):
     elif 2 in f.shape:  # f of shape (N, 2) or (2, N)
         if f.shape[1] is not 2:
             f = f.T
-    elif np.squeeze(f).shape == (4,):  # (fstart, fend, fwidth, fstep)
+    elif np.squeeze(f).shape == (4,):  # (f_start, f_end, f_width, f_step)
         f = _pair_vectors(*tuple(np.squeeze(f)))
     else:  # Sequential
         f = f.reshape(-1)
@@ -211,14 +204,14 @@ def _check_freq(f):
     return f
 
 
-def _pair_vectors(fstart, fend, fwidth, fstep):
+def _pair_vectors(f_start, f_end, f_width, f_step):
     # Generate two array for phase and amplitude :
-    fdown = np.arange(fstart, fend - fwidth, fstep)
-    fup = np.arange(fstart + fwidth, fend, fstep)
+    fdown = np.arange(f_start, f_end - f_width, f_step)
+    fup = np.arange(f_start + f_width, f_end, f_step)
     return np.c_[fdown, fup]
 
 
-def pac_trivec(fstart=60., fend=160., fwidth=10.):
+def pac_trivec(f_start=60., f_end=160., f_width=10.):
     """Generate triangular vector.
 
     By contrast with the pac_vec function, this function generate frequency
@@ -226,11 +219,11 @@ def pac_trivec(fstart=60., fend=160., fwidth=10.):
 
     Parameters
     ----------
-    fstart : float | 60.
+    f_start : float | 60.
         Starting frequency.
-    fend : float | 160.
+    f_end : float | 160.
         Ending frequency.
-    fwidth : float | 10.
+    f_width : float | 10.
         Frequency bandwidth increase between each band.
 
     Returns
@@ -240,7 +233,7 @@ def pac_trivec(fstart=60., fend=160., fwidth=10.):
     tridx : array_like
         The triangular index for the reconstruction.
     """
-    starting = np.arange(fstart, fend + fwidth, fwidth)
+    starting = np.arange(f_start, f_end + f_width, f_width)
     f, tridx = np.array([]), np.array([])
     for num, k in enumerate(starting[0:-1]):
         # Lentgh of the vector to build :
@@ -253,17 +246,3 @@ def pac_trivec(fstart=60., fend=160., fwidth=10.):
         tridx = np.concatenate((tridx, idx), axis=0) if tridx.size else idx
         f = np.concatenate((f, fst), axis=0) if f.size else fst
     return f, tridx
-
-
-if __name__ == '__main__':
-    from tensorpac import Pac
-    data, time = pac_signals_wavelet(f_pha=10, f_amp=100, noise=1., n_trials=100,
-                                     n_pts=2000)
-    p = Pac(idpac=(4, 0, 0), fpha=(2, 20, 1, 1), famp=(60, 150, 5, 5),
-            dcomplex='wavelet', width=12)
-    pacmap = p.filterfit(1024, data, axis=2).squeeze()
-    print(pacmap.shape)
-    p.comodulogram(pacmap.mean(-1))
-    import matplotlib.pyplot as plt
-    plt.show()
-    print(data.shape)
