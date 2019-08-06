5ad1cc4e9128c2c23dbc9fd9863d7dd5ed040ffa
EtienneCmb <e.combrisson@gmail.com>
2019-08-06 11:17:35 +0000

    Make install

--- _downloads/96ce4859824f2ed391a0d841d9a106d2/plot_psd.py
+++ _downloads/96ce4859824f2ed391a0d841d9a106d2/plot_psd.py
@@ -10,8 +10,6 @@ there is the presence of a clear peak, espacially for frequency phase.
 from tensorpac import pac_signals_tort
 from tensorpac.utils import PSD
 
-import matplotlib.pyplot as plt
-
 # Dataset of signals artificially coupled between 10hz and 100hz :
 n_epochs = 20
 n_times = 4000
@@ -25,8 +23,8 @@ data, time = pac_signals_tort(f_pha=10, f_amp=100, noise=4, n_epochs=n_epochs,
 psd = PSD(data, sf)
 
 # plot the mean PSD across trials between [5, 150]Hz with a 95th confidence
-# interval
-ax = psd.plot(confidence=95, f_min=5, f_max=150)
+# interval. Also, use a log x-scale
+ax = psd.plot(confidence=95, f_min=5, f_max=150, log=True, grid=True)
 
 # finally display the figure
 psd.show()
--- _sources/auto_examples/plot_readme.rst.txt
+++ _sources/auto_examples/plot_readme.rst.txt
@@ -49,7 +49,7 @@ Reproduced the figure in the README.
 
 .. rst-class:: sphx-glr-timing
 
-   **Total running time of the script:** ( 0 minutes  4.635 seconds)
+   **Total running time of the script:** ( 0 minutes  4.295 seconds)
 
 
 .. _sphx_glr_download_auto_examples_plot_readme.py:
