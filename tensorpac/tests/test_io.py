b765e712335705e3e44b97eb9e6477372d7eb7b3
EtienneCmb <e.combrisson@gmail.com>
2019-08-20 10:56:02 +0200

    test I/O

---
+++ tensorpac/tests/test_io.py
@@ -0,0 +1,38 @@
+"""Test I/O."""
+import logging
+
+from tensorpac.io import (set_log_level, is_pandas_installed,
+                          is_statsmodels_installed, progress_bar)
+
+logger = logging.getLogger('tensorpac')
+
+
+levels = ['profiler', 'debug', 'info', 'warning', 'error', 'critical']
+
+
+class TestIO(object):
+    """Test statistical functions."""
+
+    def test_log_level(self):
+        """Test setting the log level."""
+        for l in levels:
+            set_log_level(l)
+        set_log_level(True)
+        set_log_level(False)
+
+    def test_logger(self):
+        """Test logger levels."""
+        logger.profiler("profiler")
+        logger.debug("debug")
+        logger.info("info")
+        logger.warning("warning")
+        logger.critical("critical")
+
+    def test_progress_bar(self):
+        """Test progress bar."""
+        progress_bar(5, 10)
+
+    def test_dependance(self):
+        """Test dependancies."""
+        is_statsmodels_installed()
+        is_pandas_installed()
