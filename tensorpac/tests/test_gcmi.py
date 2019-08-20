91d98369a59e58549520a8ffa1ae39c89d7a7ba4
EtienneCmb <e.combrisson@gmail.com>
2019-08-20 11:57:54 +0200

    Test GCMI

---
+++ tensorpac/tests/test_gcmi.py
@@ -0,0 +1,27 @@
+"""Test gcmi related functions."""
+import numpy as np
+
+from tensorpac.gcmi import copnorm, nd_mi_gg
+from tensorpac.config import CONFIG
+
+
+class TestGcmi(object):
+    """Test GCMI functions."""
+
+    def test_copnorm(self):
+        """Test Gaussian-copula normalization."""
+        x = np.random.rand(10, 20, 30)
+        copnorm(x)
+
+    def test_mi(self):
+        """Test computing MI."""
+        x = np.random.rand(10, 20)
+        y = np.random.rand(10, 20)
+        # basic config
+        nd_mi_gg(x, y)
+        # modified config
+        CONFIG['MI_DEMEAN'] = True
+        CONFIG['MI_BIASCORRECT'] = True
+        nd_mi_gg(x, y)
+        CONFIG['MI_BIASCORRECT'] = False
+        CONFIG['MI_DEMEAN'] = False
