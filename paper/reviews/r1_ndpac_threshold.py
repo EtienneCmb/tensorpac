47c3213cf1b0b559d3b00893c230775ef7650997
EtienneCmb <e.combrisson@gmail.com>
2020-07-02 11:14:32 +0200

    ndPAC clarifications

---
+++ paper/reviews/r1_ndpac_threshold.py
@@ -0,0 +1,85 @@
+"""#R1 : threshold of the ndPAC.
+
+This script provide an insight of the ndPAC's threshold.
+"""
+import json
+with open("../paper.json", 'r') as f: cfg = json.load(f)  # noqa
+
+import numpy as np
+from scipy.special import erfinv
+
+from tensorpac.signals import pac_signals_tort, pac_signals_wavelet
+from tensorpac import Pac
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+plt.style.use('seaborn-poster')
+sns.set_style("white")
+plt.rc('font', family=cfg["font"])
+
+
+
+def custom_ndpac(pha, amp, p=.05):
+    npts = amp.shape[-1]
+    # Normalize amplitude :
+    # Use the sample standard deviation, as in original Matlab code from author
+    amp = np.subtract(amp, np.mean(amp, axis=-1, keepdims=True))
+    amp = np.divide(amp, np.std(amp, ddof=1, axis=-1, keepdims=True))
+    # Compute pac :
+    pac = np.abs(np.einsum('i...j, k...j->ik...', amp, np.exp(1j * pha)))
+
+    s = pac ** 2
+    pac /= npts
+    # Set to zero non-significant values:
+    xlim = npts * erfinv(1 - p)**2
+
+    pac_nt = pac.copy()
+    pac[s <= 2 * xlim] = np.nan
+
+    return pac_nt.squeeze(), pac.squeeze(), s.squeeze()
+
+
+
+
+if __name__ == '__main__':
+    ###########################################################################
+    p = .05
+    sf = 1024
+    n_epochs = 1
+    n_times = 100000
+    ###########################################################################
+
+    data, time = pac_signals_wavelet(sf=sf, f_pha=10, f_amp=100, noise=2.,
+                                     n_epochs=n_epochs, n_times=n_times)
+
+
+    # extract the phase and the amplitude
+    p_obj = Pac(idpac=(1, 0, 0), f_pha=cfg["phres"], f_amp=cfg["ahres"])
+    # p_obj = Pac(idpac=(1, 0, 0), f_pha='mres', f_amp='mres')
+    pha = p_obj.filter(sf, data, ftype='phase', n_jobs=1)
+    amp = p_obj.filter(sf, data, ftype='amplitude', n_jobs=1)
+    print(pha.shape)
+
+    # compute PAC (outside of the PAC object)
+    pac_nt, pac, s = custom_ndpac(pha, amp, p=p)
+
+    plt.figure(figsize=(22, 6))
+    plt.subplot(131)
+    p_obj.comodulogram(pac_nt, cblabel="", title='Non-thresholded PAC',
+                       cmap=cfg["cmap"], vmin=0, colorbar=True)
+    plt.subplot(132)
+    p_obj.comodulogram(s, cblabel="", title='Threshold', cmap=cfg["cmap"],
+                       vmin=0, colorbar=True)
+    plt.ylabel('')
+    plt.subplot(133)
+    p_obj.comodulogram(pac, cblabel="", title='Thresholded PAC',
+                       cmap=cfg["cmap"], vmin=0, colorbar=True)
+    plt.ylabel('')
+    plt.tight_layout()
+
+    plt.savefig(f"{cfg['rpath']}/r1_ndpac_threshold.png", dpi=300,
+                bbox_inches='tight')
+
+    plt.show()
---
+++ paper/reviews/r1_zscore_amp_ndpac.py
@@ -0,0 +1,62 @@
+"""#R1 : assumptions of the ndPAC.
+
+ndPAC make the assumptions that the phase is normally distributed and the
+z-score amplitude is normally distributed. This script illustrate how those
+assumptions hold.
+
+"""
+import json
+with open("../paper.json", 'r') as f: cfg = json.load(f)  # noqa
+
+from tensorpac.signals import pac_signals_tort, pac_signals_wavelet
+from tensorpac import Pac
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+plt.style.use('seaborn-poster')
+sns.set_style("white")
+plt.rc('font', family=cfg["font"])
+
+
+###############################################################################
+sf = 1024
+n_epochs = 1
+n_times = 100000
+n_bins = 30
+###############################################################################
+
+data, time = pac_signals_wavelet(sf=sf, f_pha=10, f_amp=100, noise=2.,
+                                 n_epochs=n_epochs, n_times=n_times)
+
+
+# extract the phase and the amplitude
+p = Pac(idpac=(1, 0, 0), f_pha=[8, 12], f_amp=[60, 140])
+pha = p.filter(sf, data, ftype='phase', n_jobs=1).squeeze()
+amp = p.filter(sf, data, ftype='amplitude', n_jobs=1).squeeze()
+
+# z-score normalize the amplitude
+amp_n = (amp - amp.mean()) / amp.std()
+
+print(pha.shape, amp.shape)
+
+plt.figure(figsize=(18, 5))
+
+plt.subplot(131)
+plt.hist(pha, n_bins, color='blue')
+plt.title('Binned phase')
+
+plt.subplot(132)
+plt.hist(amp, n_bins, color='red')
+plt.title('Binned amplitude')
+
+plt.subplot(133)
+plt.hist(amp_n, n_bins, color='orange')
+plt.title('Binned z-scored amplitude')
+
+
+plt.savefig(f"{cfg['rpath']}/r1_ndpac_assumptions.png", dpi=300,
+            bbox_inches='tight')
+
+plt.show()
