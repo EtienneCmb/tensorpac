f07e66efa70a80af237af5f6c216df7c4a3da760
EtienneCmb <e.combrisson@gmail.com>
2020-07-03 14:37:25 +0200

    better lisibility of titles in Fig3

--- paper/fig_3_compare_pac.py
+++ paper/fig_3_compare_pac.py
@@ -19,10 +19,10 @@ sf = 1024
 n_epochs = 20
 n_times = 3000
 n_perm = 20
-titles = ["MVL", "KLD", "HR", "ndPAC", "PS"]
+titles = ["MVL", "KLD", "HR", "ndPAC", "PLV"]
 methods = dict(MVL=[0, slice(0, 2)], KLD=[0, slice(2, 4)],
                HR=[0, slice(4, 6)], ndPAC=[1, slice(1, 3)],
-               PS=[1, slice(3, 5)])
+               PLV=[1, slice(3, 5)])
 ###############################################################################
 
 data, time = pac_signals_wavelet(sf=sf, f_pha=10, f_amp=100, noise=2.,
@@ -30,8 +30,7 @@ data, time = pac_signals_wavelet(sf=sf, f_pha=10, f_amp=100, noise=2.,
 
 
 p = Pac(idpac=(1, 2, 3), f_pha=cfg["phres"], f_amp=cfg["ahres"])
-# p = Pac(idpac=(1, 2, 3), f_pha=(1, 30, 1, 1), f_amp=(60, 160, 5, 5))
-# p = Pac(idpac=(1, 2, 3), f_pha='mres', f_amp='mres')
+# p = Pac(idpac=(1, 2, 3), f_pha='lres', f_amp='lres')
 pha = p.filter(sf, data, ftype='phase', n_jobs=1)
 amp = p.filter(sf, data, ftype='amplitude', n_jobs=1)
 
@@ -45,8 +44,9 @@ for k in range(5):
 
     sq = methods[titles[k]]
     plt.subplot(gs[sq[0], sq[1]])
-    p.comodulogram(xpac, cblabel="", title=titles[k], cmap=cfg["cmap"],
-                   vmin=0, colorbar=True)
+    p.comodulogram(xpac, cblabel="", title=p.method.replace(' (', '\n('),
+                   cmap=cfg["cmap"], vmin=0, colorbar=True, fz_labels=14,
+                   fz_title=16)
     if k > 0:
         plt.ylabel("")
         # plt.tick_params(axis='y', which='both', labelleft=False)
