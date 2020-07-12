55cde0201dd79263d0ad438e1d46c32b6dfc0e72
EtienneCmb <e.combrisson@gmail.com>
2020-07-12 11:19:38 +0200

    fix number of permutations

--- examples/tutorials/plot_real_data.py
+++ examples/tutorials/plot_real_data.py
@@ -282,7 +282,7 @@ plt.show()
 # for computing the permutations
 p_obj.idpac = (6, 2, 0)
 # compute pac and 200 surrogates
-pac_prep = p_obj.fit(pha_p[..., time_prep], amp_p[..., time_prep], n_perm=50)
+pac_prep = p_obj.fit(pha_p[..., time_prep], amp_p[..., time_prep], n_perm=200)
 # get the p-values
 pvalues = p_obj.infer_pvalues(p=0.05, mcp='maxstat')
 
