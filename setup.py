677bcf0cc7426f558a2d84990c2ea1c068509730
EtienneCmb <e.combrisson@gmail.com>
2020-07-30 14:15:27 +0200

    v0.6.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = "0.6.4"
+__version__ = "0.6.5"
 NAME = 'tensorpac'
 AUTHOR = "Etienne Combrisson"
 MAINTAINER = "Etienne Combrisson"
